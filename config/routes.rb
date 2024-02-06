9e381398ee78e4ee56f45dd10c87c4f04b6c24ca
Steve Sherk <steven.sherk@kellogg.northwestern.edu>
2024-02-06 17:27:28 +0000

    finished class 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get "/tacos", :controller => "tacos", :action => "index"
+  get "/dice", :controller => "dice", :action => "index"
+  resources "companies"
+  # the above line is short-hand for the following: 
+    # get "/companies", :controller => "companies", :action => "index"
+  resources "contacts"
 end
